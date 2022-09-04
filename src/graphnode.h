a005ebd0606919843aa63216371deee870e030ba
raisulzaeem <raisulzaeem@gmail.com>
2022-09-05 01:47:45 +0200

    _chatBot in GraphNode class is a ChatBot object, not a pointer

--- src/graphnode.h
+++ src/graphnode.h
@@ -21,7 +21,7 @@ private:
 
     // data handles (not owned)
     std::vector<GraphEdge *> _parentEdges; // edges to preceding nodes 
-    std::unique_ptr<ChatBot> _chatBot;
+    ChatBot _chatBot;
 
     ////
     //// EOF STUDENT CODE
